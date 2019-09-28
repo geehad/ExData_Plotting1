fbc807d3ad0e4605fffe99778801597711f4ed52
unknown <gehadmohsen9519@gmail.com>
2019-09-28 19:44:14 +0200

    Adjust dimensions of the image

--- plot1.R
+++ plot1.R
@@ -5,6 +5,6 @@ df_total <- read.table(file = dataFile,header = TRUE,sep = ';')
 df_subset <- df_total[df_total$Date %in% c("1/2/2007","2/2/2007") ,]
 
 Global_active_power_numeric <- as.numeric(df_subset$Global_active_power)
-png("plot1.png")
+png("plot1.png",height = 480, width = 480)
 hist(Global_active_power_numeric, col="red", xlab="Global Active Power (kilowatts)",main = "Global Active Power")
 dev.off()
